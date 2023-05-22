eefa5f8f5cad1d2397f367a3e6fac197bd781400
bmuday <bmuday@live.fr>
2023-05-22 15:09:24 +0200

    create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Ihechikara");
+  const changeName = () => {
+    setName("Chikara");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>My name is {name}</p>
+      <button onClick={changeName}> Click me </button>
     </div>
   );
 }
